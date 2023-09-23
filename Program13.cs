4328c7e664852bf54d2892265208cb149e424449
Yuriy Nikolskiy <bankirerbe@gmail.com>
2023-09-24 00:38:52 +0300

    first commit

---
+++ Program13.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter a number: ");
+string number = Console.ReadLine();
+int number1 = Int32.Parse(number);
+
+if(number1>99)
+{
+    Console.WriteLine(number[2]);
+}
+else
+{
+    Console.WriteLine("There is no third digit");
+}
